072d282af9b617b8ef496eb96c2bcbb439511959
Vadim <v.zhukov@finik.pro>
2019-02-17 21:05:26 +0200

    Add crashlytics

--- app/src/main/java/com/vm/iqosmap/App.kt
+++ app/src/main/java/com/vm/iqosmap/App.kt
@@ -1,20 +1,37 @@
 package com.vm.iqosmap
 
 import android.app.Application
+import com.crashlytics.android.Crashlytics
+import com.crashlytics.android.core.CrashlyticsCore
 import com.vm.iqosmap.di.component.AppComponent
 import com.vm.iqosmap.di.component.DaggerAppComponent
 import com.vm.iqosmap.di.module.AppModule
+import io.fabric.sdk.android.Fabric
 
-class App: Application() {
+
+class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
 
         initDagger()
+        setUpCrashlytics()
+
+        Crashlytics.getInstance().crash() // Force a crash
     }
 
     private fun initDagger() {
         appComponent = DaggerAppComponent.builder().appModule(AppModule(this)).build()
+    }
+
+    private fun setUpCrashlytics() {
+        // Set up Crashlytics, disabled for debug builds
+        val crashlyticsKit = Crashlytics.Builder()
+            .core(CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build())
+            .build()
+
+        // Initialize Fabric with the debug-disabled crashlytics.
+        Fabric.with(this, crashlyticsKit)
     }
 
     companion object {
