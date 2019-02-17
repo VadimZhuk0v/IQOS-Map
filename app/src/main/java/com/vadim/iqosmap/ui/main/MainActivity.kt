88e7f24ebf6578738ad565e70f7f8377fbe00319
Vadim <v.zhukov@finik.pro>
2019-02-17 19:43:19 +0200

    Change app icon

--- app/src/main/java/com/vadim/iqosmap/ui/main/MainActivity.kt
+++ app/src/main/java/com/vadim/iqosmap/ui/main/MainActivity.kt
@@ -1,25 +1,9 @@
 package com.vadim.iqosmap.ui.main
 
 import android.annotation.SuppressLint
-import android.content.Intent
-import android.net.Uri
-import android.os.Bundle
-import android.view.View
-import androidx.constraintlayout.widget.ConstraintSet
-import androidx.lifecycle.Observer
-import androidx.transition.ChangeBounds
-import androidx.transition.TransitionManager
-import com.vadim.iqosmap.BuildConfig
 import com.vadim.iqosmap.R
 import com.vadim.iqosmap.base.BaseActivity
 import com.vadim.iqosmap.databinding.ActivityMainBinding
-import com.vadim.iqosmap.ui.filter.FilterFragment
-import com.vadim.iqosmap.ui.filter.IFilterCallBack
-import com.vadim.iqosmap.ui.map.MapFragment
-import com.vadim.iqosmap.ui.place.PlaceBottomSheetDialog
-import com.vadim.iqosmap.utils.extentions.addFinishListner
-import com.vadim.iqosmap.utils.ui.ConstraintSetUtils
-import com.vadim.iqosmap.utils.ui.UiUtils
 
 private const val ACTIVITY_LAYOUT_ID = R.layout.activity_main
 
